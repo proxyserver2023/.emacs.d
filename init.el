f8bc3cddef7fe6545000ed820437d82a8b1a9725
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2017-05-30 17:26:02 +0600

    Added and enabled melpa

---
+++ init.el
@@ -0,0 +1,12 @@
+;; This is custom config by
+;; alamin
+
+;; Add and enable MELPA
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+
+
+
