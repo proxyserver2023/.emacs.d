55409e8f13c4d3284ef7a117f9b7e42a84326b25
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2017-05-30 15:36:57 +0600

    Deleted Everything

--- elpa/counsel-20170503.852/counsel-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "counsel" "20170503.852" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
--- elpa/try-20170226.805/try-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "try" "20170226.805" "Try out Emacs packages." '((emacs "24")) :url "http://github.com/larstvei/try" :keywords '("packages"))
