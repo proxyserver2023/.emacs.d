0c93f4cbce9decd4e4efc7bc79aa59ee0870a02e
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2017-12-08 16:55:08 +0600

    upto: purcel-benchmarking

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;;; -*- lexical-binding: t -*-
 ;;; init.el --- Custom Emacs Configuration
 ;;; Author: Md. Alamin Mahamud
 ;;; Version: 3.0.0
@@ -8,16 +9,12 @@
 ;;; Commentary
 ;;; Code:
 
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 (require 'ob-tangle)
-(org-babel-load-file(
+(org-babel-load-file (
 		      expand-file-name
-		      "~/.emacs.d/README.org"))
+		      "README.org"
+		      user-emacs-directory))
 
 ;;; init.el ends here
