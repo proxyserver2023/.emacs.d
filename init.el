02290583a775aa5f0c154313174819595b4a9d5a
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-04-10 22:07:51 +0600

    update: wow! config is now great!
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- init.el
+++ init.el
@@ -8,6 +8,20 @@
 ;;; URL: https://github.com/AlaminMahamud/.emacs.d/init.el
 
 ;;; Code:
+;;;
+(if
+    (fboundp 'menu-bar-mode)
+    (menu-bar-mode -1))
+
+(if
+    (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+
+(if
+    (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+
+(setq inhibit-startup-message t)
 
 (package-initialize)
 
